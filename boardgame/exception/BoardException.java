0a5d2e8fa1291d9ad6bddec34519ef593d5a2f2a
GuiJPG <oguilherme.rodrigues10@gmail.com>
2024-07-31 23:51:59 -0300

    BoardException and defensive programming

---
+++ boardgame/exception/BoardException.java
@@ -0,0 +1,9 @@
+package boardgame.exception;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+
+}
--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -1,7 +1,8 @@
 package chess;
 
-import bordgame.Board;
-import bordgame.Position;
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 /**
@@ -28,5 +29,7 @@ public class ChessMatch {
 
     private void initialSetup() {
         board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }
