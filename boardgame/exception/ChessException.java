f46492f54697f3b7096af9f08de62a8a231a2753
GuiJPG <oguilherme.rodrigues10@gmail.com>
2024-08-22 11:34:24 -0300

    Moving Pieces

--- boardgame/exception/ChessException.java
+++ boardgame/exception/ChessException.java
@@ -1,6 +1,6 @@
 package boardgame.exception;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 
     public ChessException(String msg){
         super(msg);
